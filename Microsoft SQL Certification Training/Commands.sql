56e1c1c15e8b977c992ba61a5168b461a09d3772
Dev Dutt <30376826+duttdev489@users.noreply.github.com>
2021-02-27 01:23:49 +0530

    Update Commands.sql

--- Microsoft SQL Certification Training/Commands.sql
+++ Microsoft SQL Certification Training/Commands.sql
@@ -6,3 +6,34 @@ use <databasename> ;
 
 #drop database name
 drop database <databasename>
+
+
+#Data Types in SQL
+
+Numerical Data Type
+  bigint
+
+  int
+
+  smallint
+
+  tinyint
+
+  decimals(s,d)
+  
+  
+Character Datatype
+  chars(s)   --> 255 characters   
+    --> waste of memory , size 30 , given 3 chars --> 30 memory will be used
+  varchar(s) -->  255 characters
+   --> here only 3 chars memory will be used.
+  text   ---> 65535 characters  
+  
+  Date and Time Data Type
+  
+  date  yyyy-mm-dd
+  
+  time  hh:mm:ss
+  
+  year  yyyy
+  
